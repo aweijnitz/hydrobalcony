d7e3143ccd9dbef9e3b800b20ceb84ef42a33c04
Anders Weijnitz <anders.weijnitz@my-email.com>
2015-04-24 10:37:16 +0200

    Added simple HTTP API for pump control

--- RaspberryPi/ttyDataServer.js/lib/dev/MockSerialPort.js
+++ RaspberryPi/ttyDataServer.js/lib/dev/MockSerialPort.js
@@ -31,7 +31,7 @@ MockSerialPort.prototype.drain = function (cb) {
 
 
 MockSerialPort.prototype.write = function (buffer, cb) {
-    process.stdout.write('SERIAL::WRITE '+buffer.toString()+'\n');
+    process.stdout.write('MockSerialPort --> SERIAL::WRITE '+buffer.toString()+'\n');
     if (typeof cb === 'function') cb();
 };
 
