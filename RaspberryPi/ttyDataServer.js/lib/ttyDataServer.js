d5763449647c34e6db91f942c4ca58aad7938a23
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-05-07 21:05:59 +0200

    Added CORS headers

--- RaspberryPi/ttyDataServer.js/lib/ttyDataServer.js
+++ RaspberryPi/ttyDataServer.js/lib/ttyDataServer.js
@@ -44,6 +44,12 @@ var setupServer = function setupServer(appConf, log4js) {
 
 
     logger.info('Setting application routes');
+    // CORS
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
     var routes = require('../routes/index')(appConf, log4js, express.Router());
     app.use('/', routes);
 
