fee18ba43799924405911868161459954f6a1211
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-06-06 13:18:30 +0200

    new ui skeleton

--- FrontendServer/UI/app/scripts/main.js
+++ FrontendServer/UI/app/scripts/main.js
@@ -1,22 +1,15 @@
 'use strict';
 
-(function UIapp($, io, MG) {
+(function UIapp($, io) {
   var log = function (msg) {
-    console.log(msg);
+    //console.log(msg);
   };
 
-  var renderGraph = function(title, data, target) {
-    var d0 = data.map(function(i) { var o = i; o.timestamp = new Date(i.timestamp); return o; });
-    MG.data_graphic({
-      title: title || '',
-      description: '',
-      data: [d0],
-      width: 400,
-      height: 250,
-      target: target,
-      x_accessor: "timestamp",
-      y_accessor: "value",
-      interpolate: "monotone"
+  var renderGraph = function (title, data, target) {
+    var d0 = data.map(function (i) {
+      var o = i;
+      o.timestamp = new Date(i.timestamp);
+      return o;
     });
   };
 
@@ -25,8 +18,10 @@
   };
 
   var subscribe = function () {
-    var host = 'http://andersw.info:7979/';
-    var socket = io.connect(host);
+    // http://hydro.weekendhack.it/dashboard/latest/airTemp
+    var host = 'http://hydro.weekendhack.it'; // TODO: Move to conf
+    var opts = {path: '/dashboard/socket.io'}; // TODO: Move to conf
+    var socket = io.connect(host, opts);
     socket.on('data', function (data) {
       log(data);
 
@@ -34,13 +29,14 @@
     });
     socket.on('pump', function (data) {
       renderValue('pumpState', data.state, '');
+      log(data);
     });
-    socket.on('clientConnect', function(data) {
+    socket.on('clientConnect', function (data) {
       log(data);
     });
 
     var url = 'http://andersw.info:7979/sensordata/waterTemp?limit=13000';
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function (data) {
       document.data = data;
       renderGraph('Water Temp', data, document.getElementById('graph'));
 
@@ -49,4 +45,4 @@
 
   subscribe();
 
-})($, io, MG);
+})($, io);
