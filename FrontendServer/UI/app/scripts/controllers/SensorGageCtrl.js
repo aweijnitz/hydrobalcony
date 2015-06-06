fbc35e3acaed8ee755110b24599e88d845d80f26
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-06-06 23:08:13 +0200

    all functionality done. error handling next

--- FrontendServer/UI/app/scripts/controllers/SensorGageCtrl.js
+++ FrontendServer/UI/app/scripts/controllers/SensorGageCtrl.js
@@ -1,17 +1,25 @@
-app.controller('SensorGageCtrl', function ($scope, socket) {
+app.controller('SensorGageCtrl', function ($scope, $http, socket) {
   $scope.value = 0;
   $scope.init = function(name) {
     //This function is sort of private constructor for controller
     $scope.title = name;
     $scope.sensorName = name;
+    var url = "http://hydro.weekendhack.it/dashboard/latest/"+name;
+    $http.get(url).
+      success(function (data, status, headers, config) {
+        $scope.value = data.value;
+        socket.on('data', function (msg) {
+          if(name === msg.name) {
+            $scope.value = msg.value;
+          }
+        });
 
-  //  console.log('sensor: ' + name);
-    socket.on('data', function (msg) {
-      if(name === msg.name) {
-//        console.log($scope.sensorName + ' - ' + JSON.stringify(msg));
-        $scope.value = msg.value;
-      }
-    });
+      }).
+      error(function (data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
   };
 
 });
