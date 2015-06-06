f02b4418f5698ddcfa3c15d40a6ac47e70a783f5
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-06-06 20:45:14 +0200

    wip

--- FrontendServer/UI/app/scripts/dashboardApp.js
+++ FrontendServer/UI/app/scripts/dashboardApp.js
@@ -1,1 +1,9 @@
-var app = angular.module("dashboardApp", ['ngJustGage']);
+var host = 'http://hydro.weekendhack.it'; // TODO: Move to conf
+var opts = {path: '/dashboard/socket.io'}; // TODO: Move to conf
+
+var app = angular.module("dashboardApp", ['btford.socket-io', 'ngJustGage'])
+  .factory('socket', function (socketFactory) {
+    return socketFactory({
+      ioSocket: io.connect(host, opts)
+    });
+  });
